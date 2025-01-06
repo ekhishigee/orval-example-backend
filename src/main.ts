4302c688cb5303c166ed9683d2dbb44ae105b8cf
Khishigdorj Erdenebat <knownkhishigee@gmail.com>
2025-01-06 23:48:22 +0800

    refactor code

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,8 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors();
+
   const config = new DocumentBuilder()
     .setTitle('Tasks example')
     .setDescription('The tasks API description')
